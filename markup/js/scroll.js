5d7e7d01a462e6d9064abae396e55179427a9019
Andrey-Ionel <a-ionel@ukr.net>
2020-10-29 23:01:49 +0200

    scroll hover text

--- markup/js/scroll.js
+++ markup/js/scroll.js
@@ -9,7 +9,7 @@ $.fn.scrollBottom = function(scroll){
 
 $(window).scroll(function(){
 if($(window).scrollBottom()<250){
-$('#scroll-h').css({opacity: 0}
+    $('#scroll-h').css({opacity: 0}
     );
 } else {
             $('#scroll-h').css({opacity: 1}
@@ -21,10 +21,23 @@ $('#scroll-h').css({opacity: 0}
 
 $(window).scroll(function(){
 if($(window).scrollBottom()<90){
-$('.up').css({opacity: 1}
+    $('.up').css({opacity: 1}
     );
 } else {
             $('.up').css({opacity: 0}
+    );
+
+        }
+
+})
+
+$(window).scroll(function(){
+if($(window).scrollBottom()<1500 && $(window).scrollBottom()>800){
+      $('.hover-projects').css({opacity: 1}
+    );
+}
+else {
+            $('.hover-projects').css({opacity: 0}
     );
 
         }
