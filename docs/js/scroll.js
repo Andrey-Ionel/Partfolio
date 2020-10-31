f6d4283415131d7ef6ec6f51217f4766766424cc
Andrey-Ionel <a-ionel@ukr.net>
2020-10-31 21:52:03 +0200

    add docs

---
+++ docs/js/scroll.js
@@ -0,0 +1,45 @@
+$.fn.scrollBottom = function(scroll){
+  if(typeof scroll === 'number'){
+    window.scrollTo(0,$(document).height() - $(window).height() - scroll);
+    return $(document).height() - $(window).height() - scroll;
+  } else {
+    return $(document).height() - $(window).height() - $(window).scrollTop();
+  }
+}
+
+$(window).scroll(function(){
+if($(window).scrollBottom()<250){
+    $('#scroll-h').css({opacity: 0}
+    );
+} else {
+            $('#scroll-h').css({opacity: 1}
+    );
+
+        }
+
+})
+
+$(window).scroll(function(){
+if($(window).scrollBottom()<90){
+    $('.up').css({opacity: 1}
+    );
+} else {
+            $('.up').css({opacity: 0}
+    );
+
+        }
+
+})
+
+$(window).scroll(function(){
+if($(window).scrollBottom()<1500 && $(window).scrollBottom()>800){
+      $('.hover-projects').css({opacity: 1}
+    );
+}
+else {
+            $('.hover-projects').css({opacity: 0}
+    );
+
+        }
+
+})
